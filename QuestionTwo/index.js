5519e29a234704dbbba3a7e633ee9576001d7833
atifsalim <atifsalimbhat@gmail.com>
2021-12-21 18:27:50 +0530

    first commit

---
+++ QuestionTwo/index.js
@@ -0,0 +1,22 @@
+// var number = prompt("Enter a Value", "0");
+var num1=10;
+var num2=0;
+var num3=25;
+
+if(num1>num2 && num1>num3 )
+{
+  
+        console.log("num1 is greater ");
+}
+   
+
+if(num2>num1 && num2>num3){
+
+     console.log("num2 is greater ");
+
+ }
+
+ if(num3>num1 && num3>num2){
+    console.log("num3 is greater ");
+
+ }

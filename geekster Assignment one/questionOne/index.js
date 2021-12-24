86f206534dd284eac54a6fa1299293bd22ca448c
atifbhat <94857007+atifbhat@users.noreply.github.com>
2021-12-24 16:09:53 +0530

    Add files via upload

---
+++ geekster Assignment one/questionOne/index.js
@@ -0,0 +1,13 @@
+
+
+var number = prompt("Enter a Value", "0");
+
+if(number>10)
+{
+    console.log("number is greater than 10");
+
+}
+else{
+    console.log("number is not greater than 10");
+
+}

9805a94cb0bd2ba88f1a82759cb21ee65955f05c
atifbhat <94857007+atifbhat@users.noreply.github.com>
2021-12-24 16:07:47 +0530

    Add files via upload

---
+++ assignment third 23dec/index.js
@@ -0,0 +1,38 @@
+
+
+var arrsize =prompt('Enter the size of an array');
+arrsize=parseInt(arrsize);
+var arr = [];
+
+
+
+for(var i=0; i<arrsize; i++)
+
+{
+  
+  z=i+1;
+  x=prompt(`Enter the ${z} element of an Array`);
+  x = parseInt(x);
+  arr.push(x);
+  x++;
+
+}
+    
+greatest=0;
+
+    for(j=0;j<arrsize;j++) 
+        {
+        
+            if(arr[j]>greatest){
+
+                greatest=arr[j];
+            
+            }
+            
+            
+        
+
+        }
+
+        alert(greatest);
+        console.log(greatest);

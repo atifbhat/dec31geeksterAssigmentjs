3790529d494764ad9bb9e5f175259ca9e256fdcf
atifbhat <94857007+atifbhat@users.noreply.github.com>
2022-01-01 22:03:03 +0530

    Add files via upload

--- geekster Assignment one/QuestionTwo/index.js
+++ geekster Assignment one/QuestionTwo/index.js
@@ -1,22 +1,25 @@
-// var number = prompt("Enter a Value", "0");
-var num1=10;
-var num2=0;
-var num3=25;
+alert("Enter three number to check the maximum among the given number?");
+var num1 = prompt("Enter a Value");
+num1 = parseInt(num1);
+var num2 = prompt("Enter a Value");
+num2 = parseInt(num2);
+var num3 = prompt("Enter a Value");
+num3 = parseInt(num3);
 
-if(num1>num2 && num1>num3 )
-{
-  
-        console.log("num1 is greater ");
+
+if (num1 > num2 && num1 > num3) {
+
+    alert("num1 is greater ");
 }
-   
 
-if(num2>num1 && num2>num3){
 
-     console.log("num2 is greater ");
+if (num2 > num1 && num2 > num3) {
 
- }
+    alert("num2 is greater ");
 
- if(num3>num1 && num3>num2){
-    console.log("num3 is greater ");
+}
 
- }
+if (num3 > num1 && num3 > num2) {
+    alert("num3 is greater ");
+
+}
--- geekster Assignment one/questionOne/index.js
+++ geekster Assignment one/questionOne/index.js
@@ -1,13 +1,9 @@
+var number = prompt("Enter a number to check whether the number is greater than 10?");
 
+if (number > 10) {
+    alert("number is greater than 10");
 
-var number = prompt("Enter a Value", "0");
-
-if(number>10)
-{
-    console.log("number is greater than 10");
-
-}
-else{
-    console.log("number is not greater than 10");
+} else {
+    alert("number is not greater than 10");
 
 }

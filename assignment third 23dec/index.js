e98beed2d5583e0e3f3edef475b262971504e9e5
atifbhat <94857007+atifbhat@users.noreply.github.com>
2022-01-01 22:04:33 +0530

    Add files via upload

--- assignment third 23dec/index.js
+++ assignment third 23dec/index.js
@@ -1,38 +1,36 @@
-
-
-var arrsize =prompt('Enter the size of an array');
-arrsize=parseInt(arrsize);
+alert("Take N numbers from User and find Max Using an Array");
+var arrsize = prompt('Enter the size of an array');
+arrsize = parseInt(arrsize);
 var arr = [];
 
 
 
-for(var i=0; i<arrsize; i++)
+for (var i = 0; i < arrsize; i++)
 
 {
-  
-  z=i+1;
-  x=prompt(`Enter the ${z} element of an Array`);
-  x = parseInt(x);
-  arr.push(x);
-  x++;
+
+    z = i + 1;
+    x = prompt(`Enter the ${z} element of an Array`);
+    x = parseInt(x);
+    arr.push(x);
+    x++;
 
 }
-    
-greatest=0;
 
-    for(j=0;j<arrsize;j++) 
-        {
-        
-            if(arr[j]>greatest){
+greatest = 0;
 
-                greatest=arr[j];
-            
-            }
-            
-            
-        
+for (j = 0; j < arrsize; j++) {
 
-        }
+    if (arr[j] > greatest) {
 
-        alert(greatest);
-        console.log(greatest);
+        greatest = arr[j];
+
+    }
+
+
+
+
+}
+
+alert(greatest);
+console.log(greatest);

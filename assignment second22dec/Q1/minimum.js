1f40a73b9ceb36beec198147d2af622d4ea2107c
atifbhat <94857007+atifbhat@users.noreply.github.com>
2022-01-01 22:04:08 +0530

    Add files via upload

--- assignment second22dec/Q1/minimum.js
+++ assignment second22dec/Q1/minimum.js
@@ -1,13 +1,11 @@
+alert("Q1:-Minimum among the three Numbers given by the user?");
 
-
-
-
-var num1=prompt("enter the Ist number ");		
-var num2=prompt("enter the 2nd number ");
-var num3=prompt("enter the 3rd number"); 
-var num1=parseInt(num1);
-var num2=parseInt(num2);
-var num3=parseInt(num3);
+var num1 = prompt("enter the Ist number ");
+var num2 = prompt("enter the 2nd number ");
+var num3 = prompt("enter the 3rd number");
+var num1 = parseInt(num1);
+var num2 = parseInt(num2);
+var num3 = parseInt(num3);
 console.log(`First number is -> ${num1}  `);
 console.log(`Second number is -> ${num2}  `);
 console.log(`Third number is -> ${num3}  `);
@@ -15,24 +13,16 @@ console.log(`Third number is -> ${num3}  `);
 
 
 
-if(num1<num2 && num1<num3 )
-{
-  
- console.log(`${num1} is smaller`);
-}
-   
+if (num1 < num2 && num1 < num3) {
 
-else if(num2<num1 && num2<num3){
+    alert(`${num1} is smaller`);
+} else if (num2 < num1 && num2 < num3) {
 
- console.log(`${num2} is smaller`);
+    alert(`${num2} is smaller`);
 
- }
+} else if (num3 < num1 && num3 < num2) {
+    alert(`${num3} is smaller`);
 
- else if(num3<num1 && num3<num2){
-    console.log(`${num3} is smaller`);
-
- }
- else
-{
-    console.log(`${num1},${num2} and ${num3} are same `);
+} else {
+    alert(`${num1},${num2} and ${num3} are same `);
 }
--- assignment second22dec/Q2/vovel.js
+++ assignment second22dec/Q2/vovel.js
@@ -1,28 +1,20 @@
+alert("You have to Enter the character to check whether it is a Vowel or Not?")
+var character = prompt("Enter the character to check whether it is a Vowel or Not?");
 
 
-var character=prompt("Enter the character to check whether it is a Vowel or Not?");
 
 
+if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+
+    alert(`${character} is Vovel!`);
+
+} else if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
+
+    alert(`${character} is Vovel!`);
+
+} else {
+
+    alert(`${character} is Consonent!`);
 
 
-        if(character=='a' || character=='e' || character=='i' || character=='o' || character=='u'){
-            
-            alert(`${character} is Vovel!`);
-
-        }
-
-        else if(character=='A' || character=='E' || character=='I' || character=='O' || character=='U'){
-            
-            alert(`${character} is Vovel!`);
-
-        }
-
-        else{
-            
-            alert(`${character} is Consonent!`);
-
-
-        }
-
-        
-
+}
